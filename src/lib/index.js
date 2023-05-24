e9a2cfbd0a242a9bef5f8c7bd1a4108dc7dd2275
avalon <juanmarcial.portilla@campusfp.es>
2023-05-24 02:04:26 +0200

    export index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,14 @@
 // Reexport your entry components here
+export { default as Avatar } from './components/Avatar.svelte';
 export { default as Button } from './components/Button.svelte';
+export { default as Checkbox } from './components/Checkbox.svelte';
+export { default as Completed } from './components/Completed.svelte';
+export { default as Difficulty } from './components/Difficulty.svelte';
+export { default as Input } from './components/Input.svelte';
+export { default as Like } from './components/Like.svelte';
+export { default as Link } from './components/Link.svelte';
+export { default as MenuIcon } from './components/MenuIcon.svelte';
+export { default as NotificationIcon } from './components/NotificationIcon.svelte';
+export { default as ReturnTop } from './components/ReturnTop.svelte';
+export { default as Tag } from './components/Tag.svelte';
+export { default as TextArea } from './components/TextArea.svelte';
